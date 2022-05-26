17ed7d733f1284a054b91d46a57d64d934e3b0ff
Jasin Sowandi <jasinsowandi@gmail.com>
2022-05-26 16:46:35 +0700

    ConvertSet to Array

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ colorUser1.add("Yellow");
 colorUser1.add("Pink");
 colorUser1.add("White");
 colorUser1.add("Purple");
-firstUser.favoriteColor = colorUser1;
+firstUser.favoriteColor = Array.from(colorUser1);
 
 firstUser.isHavePet = "Yes";
 
@@ -40,7 +40,7 @@ restUser1.add("Padang");
 restUser1.add("Tteok");
 restUser1.add("Sushi");
 restUser1.add("Sushi");
-firstUser.favoriteRestaurant = restUser1;
+firstUser.favoriteRestaurant = Array.from(restUser1);
 
 // Second User
 secondUser.name = "Wendy";
@@ -52,7 +52,7 @@ const colorUser2 = new Set();
 colorUser2.add("Blue");
 colorUser2.add("Black");
 colorUser2.add("Grey");
-secondUser.favoriteColor = colorUser2;
+secondUser.favoriteColor = Array.from(colorUser2);
 
 secondUser.isHavePet = "No";
 
@@ -77,7 +77,7 @@ restUser2.add("Katsu");
 restUser2.add("Geprek");
 restUser2.add("Pancake");
 restUser2.add("Eggy");
-secondUser.favoriteRestaurant = restUser2;
+secondUser.favoriteRestaurant = Array.from(restUser2);
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [firstUser, secondUser];
